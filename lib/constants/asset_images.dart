ef93c32fe8b9c51f8abb6360eeeb1ae8c70f6709
Deepak <djamwal10@gmail.com>
2023-07-09 18:10:08 +0530

    manages the images and icons in a class as object so we can use them easily and do any changes easily

---
+++ lib/constants/asset_images.dart
@@ -0,0 +1,16 @@
+class AssetsImages {
+  static AssetsImages instance = AssetsImages();
+
+  static const String _imagesPath = "assets/images";
+
+  final String welcomeImage = "$_imagesPath/welcome.png";
+}
+
+class AssetsIcons {
+  static AssetsIcons instance = AssetsIcons();
+
+  static const String _iconsPath = "assets/icons";
+
+  final String facebookIcon = "$_iconsPath/facebook.png";
+  final String googleIcon = "$_iconsPath/google.png";
+}
--- lib/screens/auth_ui/login/login.dart
+++ lib/screens/auth_ui/login/login.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../../constants/asset_images.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -6,15 +8,59 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text("Welcome"),
-          const SizedBox(
-            height: 12,
-          ),
-          const Text("Buy any item from here"),
-          Image.asset("assets/images/welcome.png"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: kToolbarHeight + 12,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              "Buy any item from here ",
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            Center(
+              child: Image.asset(AssetsImages.instance.welcomeImage),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CupertinoButton(
+                  onPressed: () {},
+                  padding: EdgeInsets.zero,
+                  child: Image.asset(
+                    AssetsIcons.instance.facebookIcon,
+                    scale: 12.0,
+                  ),
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                CupertinoButton(
+                  onPressed: () {},
+                  padding: EdgeInsets.zero,
+                  child: Image.asset(
+                    AssetsIcons.instance.googleIcon,
+                    scale: 12.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
