d0f5ee42113bc398310d42cbef76f8055a017661
Deepak <djamwal10@gmail.com>
2023-08-05 09:31:35 +0530

    "ORDER SCREEN" - ui improving - added logic to show the quantities of order. - In this we managed to show quantity of 1 order at a time. For multiple products order we will fix next.

--- lib/screens/order_screen/order_screen.dart
+++ lib/screens/order_screen/order_screen.dart
@@ -36,22 +36,105 @@ class OrderScreen extends StatelessWidget {
             }
             return ListView.builder(
                 itemCount: snapshot.data!.length,
+                padding: const EdgeInsets.all(12.0),
                 itemBuilder: (context, index) {
                   OrderModel orderModel = snapshot.data![index];
-                  return ExpansionTile(
-                    title: Text(
-                      orderModel.payment,
-                      style: const TextStyle(
-                          fontSize: 16.0, fontWeight: FontWeight.w500),
-                    ),
-                    children: <Widget>[
-                      ListTile(
-                        title: Text(
-                          orderModel.status,
-                          style: const TextStyle(fontWeight: FontWeight.w700),
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: ExpansionTile(
+                      childrenPadding: EdgeInsets.zero,
+                      tilePadding: EdgeInsets.zero,
+                      collapsedShape: const RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Color(0xFFAA27FB),
                         ),
-                      )
-                    ],
+                      ),
+                      shape: const RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Color((0xFFAA27FB)),
+                        ),
+                      ),
+                      title: Row(
+                        children: [
+                          Container(
+                            height: 155.0,
+                            width: 155.0,
+                            color: const Color.fromARGB(255, 204, 161, 231),
+                            child: Image.network(
+                              orderModel.products[0].image,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: SizedBox(
+                              height: 140,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 12.0,
+                                  left: 12.0,
+                                  right: 12.0,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.4,
+                                      child: Text(
+                                        orderModel.products[0].name,
+                                        maxLines: 1,
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 12.0,
+                                    ),
+                                    orderModel.products.length > 1
+                                        ? SizedBox.fromSize()
+                                        : Column(
+                                            children: [
+                                              Text(
+                                                "Quantity: ${orderModel.products[0].qty}",
+                                                style: const TextStyle(
+                                                  fontSize: 15.0,
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 12.0,
+                                              ),
+                                            ],
+                                          ),
+                                    Text(
+                                      "Total Price: \$${orderModel.totalPrice.toString()}",
+                                      style: const TextStyle(
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 12.0,
+                                    ),
+                                    Text(
+                                      "Order Status: ${orderModel.status}",
+                                      style: const TextStyle(
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      children: orderModel.products.length > 1
+                          ? [
+                              const Text("Hello"),
+                            ]
+                          : [],
+                    ),
                   );
                 });
           }),
